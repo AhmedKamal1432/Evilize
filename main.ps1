425256aacfb5c62e93963e6509ef687c71aa9193
sayeddomarr <sayed.omar@ejust.edu.eg>
2021-09-21 05:00:35 +0200

    Fixing main file #5

--- main.ps1
+++ main.ps1
@@ -1,2 +1,48 @@
-.\Helper\Evilize2.ps1
+#Dot-sourcing to the helper script
+. .\Helper\Evilize2.ps1
+# calling function inside the script itself 
+SuccessfulLogons    
+AdminLogonCreated   
+InstalledServices   
+ScheduledTaskCreated    
+ScheduledTaskDeleted    
+ScheduledTaskEnabled    
+ScheduledTaskDisabled   
+ScheduledTaskUpdated    
+KerberosAuthenticationRequested 
+KerberosServiceRequested    
+ComputerToValidate  
+RDPReconnected 
+RDPDisconnected 
+NetworkShareAccessed 
+NetworkShareChecked 
+ServiceCrashedUnexpect 
+ServicesStatus 
+ServiceSentStartStopControl 
+ServiceStartTypeChanged 
+SystemInstalledServices 
+WMIOperationStarted 
+WMIOperationTemporaryEssStarted 
+WMIOperationESStoConsumerBinding 
+PSModuleLogging 
+PSScriptBlockLogging 
+PSAuthneticatingUser 
+SessionCreated 
+WinRMAuthneticatingUser 
+ServerRemoteHostStarted 
+ServerRemoteHostEnded   
+PSPartialCode   
+ScheduledTasksCreated   
+ScheduledTasksUpdated   
+ScheduledTasksDeleted   
+ScheduledTasksExecuted  
+ScheduledTasksCompleted 
+RDPLocalSuccessfulLogon1 
+RDPLocalSuccessfulLogon2 
+RDPLocalSuccessfulReconnection 
+RDPConnectionEstablished    
+RDPConnectionsAttempts  
+RDPSuccessfulTCPConnections 
+
+
 
