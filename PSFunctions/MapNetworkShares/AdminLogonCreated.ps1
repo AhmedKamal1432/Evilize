a39e770c333ef2706915c43fc71eef98280d2785
MagyGamal <61229858+MagyGamal@users.noreply.github.com>
2021-09-24 23:27:10 +0200

    Update AdminLogonCreated.ps1

--- PSFunctions/MapNetworkShares/AdminLogonCreated.ps1
+++ PSFunctions/MapNetworkShares/AdminLogonCreated.ps1
@@ -5,7 +5,7 @@ function Get-AdminLogonCreated{
         [Parameter(Mandatory=$false)]
         [String] $LogID = "200"
     )
-$A = Get-WinEvent -FilterHashtable @{ Id=4672; Path = 'logs\Security.evtx' }
+$A = Get-WinEvent -FilterHashtable @{ Id=4672; Path = $Path }
 $global:AdminLogonCreatedcount=0
 $A | ForEach-Object -process{
        
