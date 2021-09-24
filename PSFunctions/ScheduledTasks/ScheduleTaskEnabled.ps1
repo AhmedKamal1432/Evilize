42a7198dd234ef0205a2e4c72ae079181e747389
MagyGamal <61229858+MagyGamal@users.noreply.github.com>
2021-09-25 00:49:54 +0200

    Update ScheduleTaskEnabled.ps1

--- PSFunctions/ScheduledTasks/ScheduleTaskEnabled.ps1
+++ PSFunctions/ScheduledTasks/ScheduleTaskEnabled.ps1
@@ -5,7 +5,7 @@ function Get-ScheduleTaskEnabled {
         [Parameter(Mandatory=$false)]
         [String] $LogID = "200"
     )
-$A= Get-WinEvent -FilterHashtable @{ Id=4700; Path = $Path }
+$A= Get-WinEvent -FilterHashtable @{ Id=4700; Path = $Path } -ErrorAction SilentlyContinue
 $global:ScheduleTaskEnabledcount=0
 $A | ForEach-Object -process{
        	
