4bfb3209c44d1ad4fbdb5cd09d89ec541f0687fb
MagyGamal <61229858+MagyGamal@users.noreply.github.com>
2021-09-26 12:46:47 +0200

    Update LateralMovementDetection.ps1

--- PSFunctions/PowerShellRemoting/LateralMovementDetection.ps1
+++ PSFunctions/PowerShellRemoting/LateralMovementDetection.ps1
@@ -7,14 +7,14 @@ function Get-LateralMovementDetection {
     )
 	
 $A= Get-WinEvent -FilterHashtable @{Id=53504 ;Path = $Path } -ErrorAction SilentlyContinue
-$global:LateralMovementDetectioncount=0
+
 $A | ForEach-Object -process{
 	
     $Logon = New-Object psobject
     $Logon | Add-Member -MemberType NoteProperty -name TimeCreated -value $_.TimeCreated
 	$Logon | Add-Member -MemberType NoteProperty -name AppDomain -value $_.properties[1].value
 	$Logon| Add-Member -MemberType NoteProperty -name EventID -value $_.Id
-	$global:LateralMovementDetectioncount++
+	
     $Logon
  }}
- "Number of LateralMovementDetection events:"+ $LateralMovementDetectioncount
+
