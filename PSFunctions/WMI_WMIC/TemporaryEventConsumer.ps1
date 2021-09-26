134f551d906110ce426a965476d58405f48e4b14
MagyGamal <61229858+MagyGamal@users.noreply.github.com>
2021-09-26 13:04:06 +0200

    Update TemporaryEventConsumer.ps1

--- PSFunctions/WMI_WMIC/TemporaryEventConsumer.ps1
+++ PSFunctions/WMI_WMIC/TemporaryEventConsumer.ps1
@@ -6,7 +6,7 @@ function Get-TemporaryEventConsumer {
         [String] $LogID = "200"
     )
 $A= Get-WinEvent -FilterHashtable @{Id=5860 ;Path = $Path } -ErrorAction SilentlyContinue
-$global:TemporaryEventConsumercount=0
+
 $A | ForEach-Object -process{
 	
     $Logon = New-Object psobject
@@ -14,9 +14,9 @@ $A | ForEach-Object -process{
 	$Logon | Add-Member -MemberType NoteProperty -name NameSpacename -value $_.properties[0].value
 	$Logon | Add-Member -MemberType NoteProperty -name Query -value $_.properties[1].value
 	$Logon | Add-Member -MemberType NoteProperty -name EventID -value $_.Id
-	$global:TemporaryEventConsumercount++
+	
     $Logon
 
 	
  }}
- "Number of TemporaryEventConsumer events:"+ $TemporaryEventConsumercount
+
