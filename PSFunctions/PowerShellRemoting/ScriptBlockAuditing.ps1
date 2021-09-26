de7ef455fa741ac2b780babc560842e0ef7ba3b7
MagyGamal <61229858+MagyGamal@users.noreply.github.com>
2021-09-26 12:47:25 +0200

    Update ScriptBlockAuditing.ps1

--- PSFunctions/PowerShellRemoting/ScriptBlockAuditing.ps1
+++ PSFunctions/PowerShellRemoting/ScriptBlockAuditing.ps1
@@ -7,14 +7,14 @@ function Get-ScriptBlockAuditing {
     )
 	
 $A= Get-WinEvent -FilterHashtable @{Id=4104 ;Path = $Path } -ErrorAction SilentlyContinue
-$global:ScriptBlockAuditingcount=0
+
 $A | ForEach-Object -process{
 	
     $Logon = New-Object psobject
     $Logon | Add-Member -MemberType NoteProperty -name TimeCreated -value $_.TimeCreated
 	$Logon | Add-Member -MemberType NoteProperty -name CommandsInvoced -value $_.properties[2].value
 	$Logon | Add-Member -MemberType NoteProperty -name EventID -value $_.Id
-	$global:ScriptBlockAuditingcount++
+	
     $Logon
  }}
- "Number of ScriptBlockAuditing events:"+ $ScriptBlockAuditingcount
+
