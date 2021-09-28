bf3e4d6f4f395f30a73716341e6fa699035a2927
MagyGamal <61229858+MagyGamal@users.noreply.github.com>
2021-09-28 12:47:25 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,4 +1,3 @@
-
 $Logs_Path = Read-Host -Prompt "Please, Enter Events logs path" 
 $securityparam= Read-Host -Prompt "Do you want to parse the security event log? yes\no"
 $Destination_Path= Join-Path -Path $Logs_Path -ChildPath "Results"
@@ -40,9 +39,6 @@ $PowerShellRemoting_Path=Join-Path -Path $Destination_Path -ChildPath "PowerShel
 if ((Test-Path -Path "$PowerShellRemoting_Path")-eq $false) {
     New-Item -Path $PowerShellRemoting_Path -ItemType Directory    
 }
-
-##Validating Paths
-$LogsPathTest=Test-Path -Path "$Logs_Path"
 
 $Security_Path= Join-Path -Path $Logs_Path -ChildPath "Security.evtx"
 $System_Path= Join-Path -Path $Logs_Path -ChildPath "System.evtx"
@@ -88,6 +84,8 @@ if (-NOT ($evtFiles -eq $null)){
 	Evt2Evtx $RemoteConnectionevt_Path $RemoteConnection_Path
 	Evt2Evtx $PowerShellOperationalevt_Path $PowerShellOperational_Path
 }
+##Validating Paths
+$LogsPathTest=Test-Path -Path "$Logs_Path"
 ## Testing if the log file exist ? 
 $Valid_Security_Path= Test-Path -Path $Security_Path
 $Valid_System_Path= Test-Path -Path $System_Path
@@ -105,6 +103,7 @@ $ResultsArray=@()
 	#Remote desktop
 		#destination#
 if ($securityparam -eq "yes") {
+ if ($Valid_Security_Path -eq $true) {
 . .\PSFunctions\RemoteDesktop\AllSuccessfulLogons.ps1
 Get-AllSuccessfulLogons -Path $Security_Path  | Export-Csv -Path $RemoteDesktop_Path\AllSuccessfulLogons.csv -NoTypeInformation 
 write-host  "Number of AllSuccessfulLogons:" , (Import-Csv $RemoteDesktop_Path\AllSuccessfulLogons.csv).count
@@ -122,9 +121,14 @@ Get-RDPDisconnected -Path $Security_Path  | Export-Csv -Path $RemoteDesktop_Path
 write-host  "Number of RDPDisconnected  events:" , (Import-Csv $RemoteDesktop_Path\RDPDisconnected.csv).count
 $hash= New-Object PSObject -property @{EventID="4779"; SANSCateogry="RemoteDesktop"; Event="RDP sessions disconnected"; NumberOfOccurences=(Import-Csv $RemoteDesktop_Path\RDPDisconnected.csv).count}
 $ResultsArray+=$hash
-
+	}
+ else{ 
+  write-host "Error: Security event log is not found" -ForegroundColor Red
+        
+ }
 }
 
+if ($Valid_RDPCORETS_Path -eq $true) {
 . .\PSFunctions\RemoteDesktop\RDPConnectionAttempts.ps1
 Get-RDPConnectionAttempts -Path $RDPCORETS_Path  | Export-Csv -Path $RemoteDesktop_Path\RDPConnectionAttempts.csv -NoTypeInformation
 write-host  "Number of RDPConnectionAttempts  events:" , (Import-Csv $RemoteDesktop_Path\RDPConnectionAttempts.csv).count
@@ -136,14 +140,21 @@ Get-RDPSuccessfulConnections -Path $RDPCORETS_Path  | Export-Csv -Path $RemoteDe
 write-host  "Number of RDPSuccessfulConnections  events:" , (Import-Csv $RemoteDesktop_Path\RDPSuccessfulConnections.csv).count
 $hash= New-Object PSObject -property @{EventID="98";SANSCateogry="RemoteDesktop"; Event="RDP Successful Connections"; NumberOfOccurences=(Import-Csv $RemoteDesktop_Path\RDPSuccessfulConnections.csv).count}
 $ResultsArray+=$hash
+}
+else{ write-host "Error: Microsoft-Windows-RemoteDesktopServices-RdpCoreTS%4Operational event log is not found" -ForegroundColor Red
+          }
 
-
+if ($Valid_RemoteConnection_Path -eq $true) {
 . .\PSFunctions\RemoteDesktop\UserAuthSucceeded.ps1
 Get-UserAuthSucceeded -Path $RemoteConnection_Path | Export-Csv -Path $RemoteDesktop_Path\UserAuthSucceeded.csv -NoTypeInformation 
 write-host  "Number of UserAuthSucceeded  events:" , (Import-Csv $RemoteDesktop_Path\UserAuthSucceeded.csv).count
 $hash= New-Object PSObject -property @{EventID="1149";SANSCateogry="RemoteDesktop"; Event="User authentication succeeded"; NumberOfOccurences=(Import-Csv $RemoteDesktop_Path\UserAuthSucceeded.csv).count}
 $ResultsArray+=$hash
+}
+else { write-host "Error: Microsoft-Windows-TerminalServices-RemoteConnectionManager%4Operational event log is not found" -ForegroundColor Red
+          }
 
+if ($Valid_TerminalServices_Path -eq $true) {
 . .\PSFunctions\RemoteDesktop\RDPSessionLogonSucceed.ps1
 Get-RDPSessionLogonSucceed -Path $TerminalServices_Path  | Export-Csv -Path $RemoteDesktop_Path\RDPSessionLogonSucceed.csv -NoTypeInformation
 write-host  "Number of RDPSessionLogonSucceed  events:" , (Import-Csv $RemoteDesktop_Path\RDPSessionLogonSucceed.csv).count
@@ -167,18 +178,27 @@ Get-RDPbeginSession -Path $TerminalServices_Path  | Export-Csv -Path $RemoteDesk
 write-host  "Number of RDPbeginSession  events:" , (Import-Csv $RemoteDesktop_Path\RDPbeginSession.csv).count
 $hash= New-Object PSObject -property @{EventID="41";SANSCateogry="RemoteDesktop"; Event="RDP Begin Session"; NumberOfOccurences=(Import-Csv $RemoteDesktop_Path\RDPbeginSession.csv).count}
 $ResultsArray+=$hash
+}
+else { write-host "Error: Microsoft-Windows-TerminalServices-LocalSessionManager%4Operational event log is not found" -ForegroundColor Red
+         }
 
 # Remote Access
 	#Remote desktop
 		#source
 if ($securityparam -eq "yes") {
+ if ($Valid_Security_Path -eq $true) {
 #. D:\EJUST\Internships\TMinternship\evilize_project\RemoteDesktopcsv\ExplicitCreds.ps1
 #Get-ExplicitCreds -Path $Security_Path  | Export-Csv -Path $RemoteDesktop_Path\ExplicitCreds.csv -NoTypeInformation
 #write-host  "Number of ExplicitCreds  events:" , (Import-Csv $RemoteDesktop_Path\ExplicitCreds.csv).count
 #$hash= New-Object PSObject -property @{EventID="4648";SANSCateogry="RemoteDesktop"; Event="Exolicit Credentials"; NumberOfOccurences=(Import-Csv $RemoteDesktop_Path\ExplicitCreds.csv).count}
 #$ResultsArray+=$hash
+	}
+ else{ 
+  write-host "Error: Security event log is not found" -ForegroundColor Red  
+ }
 }
 
+if ($Valid_RDPCORETS_Path -eq $true) {
 #. D:\EJUST\Internships\TMinternship\evilize_project\RemoteDesktopcsv\RDPConnectingtoServer.ps1
 #Get-RDPConnectingtoServer -Path $RDPCORETS_Path  | Export-Csv -Path $RemoteDesktop_Path\RDPConnectingtoServer.csv -NoTypeInformation
 #write-host  "Number of RDPConnectingtoServer  events:" , (Import-Csv $RemoteDesktop_Path\RDPConnectingtoServer.csv).count
@@ -190,13 +210,16 @@ if ($securityparam -eq "yes") {
 #write-host  "Number of RDPConnectionInitiated  events:" , (Import-Csv $RemoteDesktop_Path\RDPConnectionInitiated.csv).count
 #$hash= New-Object PSObject -property @{EventID="1102";SANSCateogry="RemoteDesktop"; Event="RDP Connection Initiated"; NumberOfOccurences=(Import-Csv $RemoteDesktop_Path\RDPConnectionInitiated.csv).count}
 #$ResultsArray+=$hash
-
+}
+else{ write-host "Error: Microsoft-Windows-RemoteDesktopServices-RdpCoreTS%4Operational event log is not found" -ForegroundColor Red
+        }
 #################################################################################################################################################3
 
 #Remote Access	
 	# Map Network Shares 
 		#destination
 if ($securityparam -eq "yes") {
+	if ($Valid_Security_Path -eq $true) {
 #. .\PSFunctions\MapNetworkShares\AllSuccessfulLogons.ps1
 #Get-AllSuccessfulLogons -Path $Security_Path  | Export-Csv -Path $MapNetworkShares_Path\AllSuccessfulLogons.csv -NoTypeInformation
 #write-host  "Number of AllSuccessfulLogons  events:" , ((Get-Content Results\MapNetworkSharescsv\AllSuccessfulLogons.csv).Length -1)
@@ -239,16 +262,13 @@ write-host  "Number of AuditingofSharedfiles  events:" , ((Import-Csv $MapNetwor
 $hash= New-Object PSObject -property @{EventID="5145";SANSCateogry="MapNetworkShares"; Event="Auditing of Shared Files"; NumberOfOccurences=(Import-Csv $MapNetworkShares_Path\AuditingofSharedfiles.csv).count}
 $ResultsArray+=$hash
 
-}
 # Remote Access 
 	#Map Network Shares
 		# source
-if ($securityparam -eq "yes") {
 #. .\MapNetworkShares\ExplicitCreds.ps1
 #Get-ExplicitCreds -Path $Security_Path  | Export-Csv -Path $MapNetworkShares_Path\ExplicitCreds.csv -NoTypeInformation
 #$hash= New-Object PSObject -property @{EventID="4648";SANSCateogry="MapNetworkShares"; Event="RDP Connection Attempts"; NumberOfOccurences=(Import-Csv $MapNetworkShares_Path\ExplicitCreds.csv).count}
 #$ResultsArray+=$hash
-}
 #FailedLogintoDestination TODO
 
 #######################################################################################################################
@@ -256,7 +276,7 @@ if ($securityparam -eq "yes") {
 #Remote Execution
 	#PsExec
 		#Destination
-if ($securityparam -eq "yes") {
+
 #. .\PSFunctions\PsExec\AllSuccessfulLogons.ps1
 #Get-AllSuccessfulLogons -Path $Security_Path  | Export-Csv -Path $PsExec_Path\AllSuccessfulLogons.csv -NoTypeInformation 
 #write-host  "Number of AllSuccessfulLogons  events:" , ((Import-Csv $PsExec_Path\AllSuccessfulLogons.csv).count)
@@ -274,28 +294,38 @@ if ($securityparam -eq "yes") {
 #write-host  "Number of NetworkShareAccessed  events:" , ((Import-Csv $PsExec_Path\NetworkShareAccessed.csv).count)
 #$hash= New-Object PSObject -property @{EventID="5140";SANSCateogry="PSExec"; Event="Network Share Accessed"; NumberOfOccurences=(Import-Csv $PsExec_Path\NetworkShareAccessed.csv).count}
 #$ResultsArray+=$hash
+	}
+	 else{ 
+  write-host "Error: Security event log is not found" -ForegroundColor Red   
+	}
 }
+if ($Valid_System_Path -eq $true) {
 . .\PSFunctions\PsExec\ServiceInstall.ps1
 Get-ServiceInstall -Path $System_Path  | Export-Csv -Path $PsExec_Path\ServiceInstall.csv -NoTypeInformation
 write-host  "Number of ServiceInstall  events:" , ((Import-Csv $PsExec_Path\ServiceInstall.csv).count)
 $hash= New-Object PSObject -property @{EventID="7045";SANSCateogry="PSExec"; Event="Installed Service"; NumberOfOccurences=(Import-Csv $PsExec_Path\ServiceInstall.csv).count}
 $ResultsArray+=$hash
+}
+else{ 
+  write-host "Error: System event log is not found" -ForegroundColor Red   
+}
 
 #Remote Execution
 	#PsExec
 		#source
 if ($securityparam -eq "yes") {
+	if ($Valid_Security_Path -eq $true) {
 #. .\PsExec\ExplicitCreds.ps1
 #Get-ExplicitCreds -Path $Security_Path  | Export-Csv -Path $PsExec_Path\ExplicitCreds.csv -NoTypeInformation
 #$hash= New-Object PSObject -property @{EventID="4648";SANSCateogry="PSExec"; Event="Exolicit Credentials"; NumberOfOccurences=(Import-Csv $PsExec_Path\ExplicitCreds.csv).count}
 #$ResultsArray+=$hash
-}
+
 ############################################################################################################################
 
 #Remote Execution
 	#Scheduled Tasks
 		#Destination
-if ($securityparam -eq "yes") {
+
 #. .\PSFunctions\ScheduledTasks\AllSuccessfulLogons.ps1
 #Get-AllSuccessfulLogons -Path $Security_Path | Export-Csv -Path $ScheduledTasks_Path\AllSuccessfulLogons.csv -NoTypeInformation 
 #write-host  "Number of AllSuccessfulLogons  events:" , ((Import-Csv $ScheduledTasks_Path\AllSuccessfulLogons.csv).count)
@@ -337,8 +367,12 @@ Get-ScheduleTaskUpdated -Path  $Security_Path | Export-Csv -Path $ScheduledTasks
 write-host  "Number of ScheduleTaskUpdated  events:" , ((Import-Csv $ScheduledTasks_Path\ScheduleTaskUpdated.csv).count)
 $hash= New-Object PSObject -property @{EventID="4702";SANSCateogry="ScheduledTasks"; Event="Schedule Task Updated"; NumberOfOccurences=(Import-Csv $ScheduledTasks_Path\ScheduleTaskUpdated.csv).count}
 $ResultsArray+=$hash
-
 }
+else{ 
+  write-host "Error: Security event log is not found" -ForegroundColor Red   
+}
+}
+if ($Valid_TaskScheduler_Path -eq $true) {
 . .\PSFunctions\ScheduledTasks\CreatingTaskSchedulerTask .ps1
 Get-CreatingTaskSchedulerTask  -Path  $TaskScheduler_Path | Export-Csv -Path $ScheduledTasks_Path\CreatingTaskSchedulerTask.csv -NoTypeInformation
 write-host  "Number of CreatingTaskSchedulerTask  events:" , ((Import-Csv $ScheduledTasks_Path\CreatingTaskSchedulerTask.csv).count)
@@ -368,21 +402,25 @@ Get-CompletingTaskSchedulerTask -Path  $TaskScheduler_Path | Export-Csv -Path $S
 write-host  "Number of CompletingTaskSchedulerTask  events:" , ((Import-Csv $ScheduledTasks_Path\CompletingTaskSchedulerTask.csv).count)
 $hash= New-Object PSObject -property @{EventID="201";SANSCateogry="ScheduledTasks"; Event="Completing TaskScheduler Task"; NumberOfOccurences=(Import-Csv $ScheduledTasks_Path\CompletingTaskSchedulerTask.csv).count}
 $ResultsArray+=$hash
-
+}
+else{ 
+  write-host "Error: Microsoft-Windows-TaskScheduler%4Maintenance event log is not found" -ForegroundColor Red   
+}
 #Remote Execution
 	#Scheduled Tasks
 		#source
 #. .\ScheduledTasks\ExplicitCreds.ps1
 if ($securityparam -eq "yes") {
+	if ($Valid_Security_Path -eq $true) {
 #Get-ExplicitCreds -Path $Security_Path  | Export-Csv -Path $ScheduledTasks_Path\ExplicitCreds.csv -NoTypeInformation
 #$hash= New-Object PSObject -property @{EventID="4648";SANSCateogry="ScheduledTasks"; Event="Explicit credentials"; NumberOfOccurences=(Import-Csv $ScheduledTasks_Path\ExplicitCreds.csv).count}
 #$ResultsArray+=$hash
-}
+
 #####################################################################################################################################
 #Remote Execution
 	#services
 		#destination
-if ($securityparam -eq "yes") {
+
 #. .\PSFunctions\Services\AllSuccessfulLogons.ps1
 #Get-AllSuccessfulLogons -Path $Security_Path  | Export-Csv -Path $Services_Path\AllSuccessfulLogons.csv -NoTypeInformation 
 #write-host  "Number of AllSuccessfulLogons  events:" , ((Import-Csv $Services_Path\AllSuccessfulLogons.csv).count)
@@ -394,7 +432,13 @@ Get-ServiceInstalledonSystem -Path $Security_Path  | Export-Csv -Path $Services_
 write-host  "Number of ServiceInstalledonSystem  events:" , ((Import-Csv $Services_Path\ServiceInstalledonSystem.csv).count)
 $hash= New-Object PSObject -property @{EventID="4697";SANSCateogry="Services"; Event="Service Installed on System"; NumberOfOccurences=(Import-Csv $Services_Path\ServiceInstalledonSystem.csv).count}
 $ResultsArray+=$hash
+	}
+	else{ 
+  write-host "Error: Security event log is not found" -ForegroundColor Red   
+	}
 }
+
+if ($Valid_System_Path -eq $true) {
 . .\PSFunctions\Services\ServiceCrashed.ps1
 Get-ServiceCrashed -Path $System_Path  | Export-Csv -Path $Services_Path\ServiceCrashed.csv -NoTypeInformation
 write-host  "Number of ServiceCrashed  events:" , ((Import-Csv $Services_Path\ServiceCrashed.csv).count)
@@ -424,7 +468,10 @@ $ResultsArray+=$hash
 #write-host  "Number of ServiceInstall  events:" , ((Import-Csv $Services_Path\ServiceInstall.csv).count)
 #$hash= New-Object PSObject -property @{EventID="7045";SANSCateogry="Services"; Event="Service Install"; NumberOfOccurences=(Import-Csv $Services_Path\ServiceInstall.csv).count}
 #$ResultsArray+=$hash
-
+}
+else{ 
+  write-host "Error: System event log is not found" -ForegroundColor Red   
+	}
 ####################################################################################################################################
 #Remote Execution
 	#WMI\WMIC
@@ -443,6 +490,7 @@ if ($securityparam -eq "yes") {
 #$ResultsArray+=$hash
 }
 
+if ($Valid_WinRM_Path -eq $true) {
 . .\PSFunctions\WMI_WMIC\SystemQueryWMI.ps1
 Get-SystemQueryWMI -Path $WinRM_Path  | Export-Csv -Path $WMIOut_Path\SystemQueryWMI.csv -NoTypeInformation
 write-host  "Number of SystemQueryWMI  events:" , ((Import-Csv $WMIOut_Path\SystemQueryWMI.csv).count)
@@ -460,7 +508,10 @@ Get-PermenantEventConsumer -Path $WinRM_Path   | Export-Csv -Path $WMIOut_Path\P
 write-host  "Number of PermenantEventConsumer  events:" , ((Import-Csv $WMIOut_Path\PermenantEventConsumer.csv).count)
 $hash= New-Object PSObject -property @{EventID="5861";SANSCateogry="WMI\WMIC"; Event="Permenant Event Consumer"; NumberOfOccurences=(Import-Csv $WMIOut_Path\PermenantEventConsumer.csv).count}
 $ResultsArray+=$hash
-
+}
+else{ 
+  write-host "Error: Microsoft-Windows-WinRM%4Operational event log is not found" -ForegroundColor Red   
+	}
 #Remote Execution
 	#WMI\WMIC
 		#source
@@ -488,6 +539,8 @@ if ($securityparam -eq "yes") {
 #$hash= New-Object PSObject -property @{EventID="4672";SANSCateogry="PowerShellRemoting"; Event="Admin Logon Created"; NumberOfOccurences=(Import-Csv $PowerShellRemoting_Path\AdminLogonCreated.csv).count}
 #$ResultsArray+=$hash
 }
+
+if ($Valid_PowerShellOperational_Path -eq $true) {
 . .\PSFunctions\PowerShellRemoting\ScriptBlockLogging.ps1
 Get-ScriptBlockLogging -Path $PowerShellOperational_Path  | Export-Csv -Path $PowerShellRemoting_Path\ScriptBlockLogging.csv -NoTypeInformation -ErrorAction SilentlyContinue
 write-host  "Number of ScriptBlockLogging  events:" , (Import-Csv $PowerShellRemoting_Path\ScriptBlockLogging.csv).count 
@@ -505,7 +558,12 @@ Get-LateralMovementDetection -Path $PowerShellOperational_Path  | Export-Csv -Pa
 write-host  "Number of LateralMovementDetection  events:" , ((Import-Csv $PowerShellRemoting_Path\LateralMovementDetection.csv).count)
 $hash= New-Object PSObject -property @{EventID="53504";SANSCateogry="PowerShellRemoting"; Event="Lateral Movement Detection"; NumberOfOccurences=(Import-Csv $PowerShellRemoting_Path\LateralMovementDetection.csv).count}
 $ResultsArray+=$hash
+}
+else{ 
+  write-host "Error: Microsoft-Windows-PowerShell%4Operational event log is not found" -ForegroundColor Red   
+	}
 
+if ($Valid_WinPowerShell_Path -eq $true) {
 . .\PSFunctions\PowerShellRemoting\StartPSRemoteSession.ps1
 Get-StartPSRemoteSession -Path $WinPowerShell_Path  | Export-Csv -Path $PowerShellRemoting_Path\StartPSRemoteSession.csv -NoTypeInformation
 write-host  "Number of StartPSRemoteSession  events:" , ((Import-Csv $PowerShellRemoting_Path\StartPSRemoteSession.csv).count)
@@ -523,7 +581,10 @@ Get-PipelineExecution -Path $WinPowerShell_Path| Export-Csv -Path $PowerShellRem
 write-host  "Number of PipelineExecution  events:" , ((Import-Csv $PowerShellRemoting_Path\PipelineExecution.csv).count)
 $hash= New-Object PSObject -property @{EventID="800";SANSCateogry="PowerShellRemoting"; Event="Pipeline Execution"; NumberOfOccurences=(Import-Csv $PowerShellRemoting_Path\PipelineExecution.csv).count}
 $ResultsArray+=$hash
-
+}
+else{ 
+  write-host "Error: Windows PowerShell event log is not found" -ForegroundColor Red   
+	}
 #Remote Execution
 	#powershell remoting
 		#source
