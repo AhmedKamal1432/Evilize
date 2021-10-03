277eabfbd9eb7d2e855625b5fdb382c59e322102
MagyGamal <61229858+MagyGamal@users.noreply.github.com>
2021-10-03 15:30:26 +0200

    Update AllSuccessfulLogons.ps1

--- PSFunctions/RemoteDesktop/AllSuccessfulLogons.ps1
+++ PSFunctions/RemoteDesktop/AllSuccessfulLogons.ps1
@@ -7,7 +7,7 @@ function Get-AllSuccessfulLogons {
     )
 $A = Get-WinEvent -FilterHashtable @{ Id=4624; Path = $Path } -ErrorAction SilentlyContinue
 
-$A | ForEach-Object -process{
+$A | ForEach-Object -Parallel{
 
     # Logon Type
     $logon_type = $_.Message.split([Environment]::NewLine)| Select-String -Pattern "(Logon Type:).*([0-9]+)" |ForEach-Object {$_.Matches[0].Groups[2].Value}
